aea5eb09fd476e308f07deecbad783d383c691f4
HLHT2025 <hoshiyomiiam@gmail.com>
2026-01-12 10:31:10 +0700

    Add files via upload

---
+++ scripts/taitrang.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Cảm ơn bạn! Chúng tôi sẽ liên hệ tư vấn trong thời gian sớm nhất.");
+});
+// JavaScript Document
